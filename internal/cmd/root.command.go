33bad0901c5830cc3a4c65c63ea257bc376d32e2
Jorge Junior <jorge.junior.568@live.com>
2023-09-29 08:16:30 -0300

    wip: default command

--- internal/cmd/root.command.go
+++ internal/cmd/root.command.go
@@ -1,10 +1,41 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/labstack/gommon/color"
+	"github.com/spf13/cobra"
+)
 
-var RootCmd = new(cobra.Command)
+var RootCmd = &cobra.Command{
+	Short: "Wheregoes is a CLI tool to track a URL",
+	Long:  "Wheregoes is a CLI tool to track a URL",
+}
 
 func init() {
-	RootCmd.AddCommand(serve())
 	RootCmd.AddCommand(track())
+	RootCmd.AddCommand(serve())
+
+	RootCmd.Flags().BoolP("version", "v", false, "Print version number")
+	RootCmd.Run = func(cmd *cobra.Command, args []string) {
+		print("args", args)
+		if cmd.Flag("version").Value.String() == "true" {
+			fmt.Printf(
+				color.Green(
+					fmt.Sprintf("Version: %s\n", "1.0.0"),
+				),
+			)
+			return
+		}
+
+		hasArgs := len(args) > 0
+		if hasArgs {
+			print(color.Red(fmt.Sprintf("Unknown command: %s\n\n", args[0])))
+			RootCmd.Commands()[0].Run(cmd, args)
+		}
+
+		err := cmd.Help()
+		if err != nil {
+			return
+		}
+	}
 }
