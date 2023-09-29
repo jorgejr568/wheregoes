90ce7938eb557ef0c564f559246f4b27a9d4495f
Jorge Junior <jorge.junior.568@live.com>
2023-09-29 08:30:12 -0300

    feat: Default command sends to tracker (#1)
    * wip: default command
    
    * feat: root default track
    
    * docs: usage of default tracker

--- internal/cmd/root.command.go
+++ internal/cmd/root.command.go
@@ -1,10 +1,44 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/labstack/gommon/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
 
-var RootCmd = new(cobra.Command)
+var TrackCmd = track()
+var ServeCmd = serve()
+
+var DefaultCommand = TrackCmd
+
+var RootCmd = &cobra.Command{
+	Short: "Wheregoes is a CLI tool to track a URL",
+	Long:  "Wheregoes is a CLI tool to track a URL",
+	Run: func(cmd *cobra.Command, args []string) {
+		if cmd.Flag("version").Value.String() == "true" {
+			fmt.Printf(
+				color.Green(
+					fmt.Sprintf("Version: %s\n", "1.0.0"),
+				),
+			)
+			return
+		}
+
+		DefaultCommand.Run(cmd, args)
+		return
+	},
+	PreRun: func(cmd *cobra.Command, args []string) {
+	},
+	Args: DefaultCommand.Args,
+}
 
 func init() {
-	RootCmd.AddCommand(serve())
-	RootCmd.AddCommand(track())
+	RootCmd.AddCommand(TrackCmd)
+	RootCmd.AddCommand(ServeCmd)
+
+	RootCmd.Flags().BoolP("version", "v", false, "Print version number")
+	DefaultCommand.Flags().VisitAll(func(flag *pflag.Flag) {
+		RootCmd.Flags().AddFlag(flag)
+	})
 }
